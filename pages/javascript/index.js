83bcd1a6c962957f4ad9f838893a76e7de1ea6b9
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-27 23:52:09 -0300

    se agregan operadores de comparación

--- pages/javascript/index.js
+++ pages/javascript/index.js
@@ -14,11 +14,51 @@ export default function Page() {
             <td>Igual</td>
           </tr>
           <tr>
+            <td>!=</td>
+            <td>No es igual</td>
+          </tr>
+          <tr>
             <td>===</td>
-            <td>Estrictamente Igual (Compara valor y tipo de dato)</td>
+            <td>Estrictamente Igual</td>
+          </tr>
+          <tr>
+            <td>!==</td>
+            <td>Desigualdad estricta</td>
+          </tr>
+          <tr>
+            <td>{'>'}</td>
+            <td>Mayor que</td>
+          </tr>
+          <tr>
+            <td>{'>='}</td>
+            <td>Mayor o igual que</td>
+          </tr>
+          <tr>
+            <td>{'<'}</td>
+            <td>Menor que</td>
+          </tr>
+          <tr>
+            <td>{'<='}</td>
+            <td>Menor o igual que</td>
           </tr>
         </tbody>
       </table>
+
+      <h2>if / else if / else</h2>
+      <pre>
+        const age = 26 if(age > 18)
+        {
+          // ...
+        }
+        else if(age === 18)
+        {
+          //...
+        }
+        else
+        {
+          // ...
+        }
+      </pre>
     </div>
   )
 }
