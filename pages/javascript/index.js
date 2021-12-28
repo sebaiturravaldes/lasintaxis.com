63ab88633d1469de7d8bd6d7511aaccc859806db
Sebastián Iturra <iturra.sebastian@hotmail.com>
2021-12-27 23:43:36 -0300

    first commit

---
+++ pages/javascript/index.js
@@ -0,0 +1,24 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Javascript</h1>
+      <h2>Operadores de comparación</h2>
+      <table>
+        <thead>
+          <th>Operador</th>
+          <th>Significado</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>==</td>
+            <td>Igual</td>
+          </tr>
+          <tr>
+            <td>===</td>
+            <td>Estrictamente Igual (Compara valor y tipo de dato)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
